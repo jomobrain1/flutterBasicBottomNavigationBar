4d0ab69ee79c43b568a53c632e234c4ec66575ae
thecodingmall <thecodingmall@gmail.com>
2022-08-08 01:02:38 -0700

    initial messages

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+import 'package:learn/chats_page.dart';
+import 'package:learn/counter_page.dart';
+import 'package:learn/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: mainApp(),
+  ));
+}
+
+class mainApp extends StatefulWidget {
+  mainApp({Key? key}) : super(key: key);
+
+  @override
+  State<mainApp> createState() => _mainAppState();
+}
+
+class _mainAppState extends State<mainApp> {
+  int currentIndex = 0;
+  final screens = [
+   HomePage(),
+  
+   Chats(),
+   Counter()
+  ];
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        
+        body:IndexedStack(
+          index: currentIndex,
+          children: screens),
+        bottomNavigationBar: BottomNavigationBar(
+            currentIndex: currentIndex,
+            onTap: (index) => setState(() => currentIndex = index),
+            backgroundColor: Colors.blue,
+            selectedItemColor: Colors.white,
+            unselectedItemColor: Colors.white60,
+            iconSize: 20,
+            selectedFontSize: 15,
+            unselectedFontSize: 13,
+            showUnselectedLabels: false,
+            items: [
+              BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+              BottomNavigationBarItem(icon: Icon(Icons.chat), label: 'Chats'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.add), label: 'counter'),
+            ]),
+      );
+}
